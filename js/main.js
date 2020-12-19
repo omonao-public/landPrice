ddb924dfdbaca33cc87d15dcf8c327cb0ba14b1b
nagashima-omonao <omonao-business@gmail.com>
2020-12-20 04:07:10 +0900

    modified

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var main = new Vue({
   methods: {
     send: function(event){
       // alert('住所は' + this.address);
-      axios.get('https://geocoding.jp/api/?q=埼玉')
+      axios.get('https://www.geocoding.jp/api/?q=%E5%9F%BC%E7%8E%89%E7%9C%8C%E5%8C%97%E6%9C%AC%E5%B8%82')
       .then(response => {
         console.log(JSON.stringify(response.data));
       })
